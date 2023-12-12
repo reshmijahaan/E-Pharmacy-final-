99596c80829bc9a1c41a5481cfe0a0240aa43938
shakil <shakilahamed0607@gmail.com>
2023-12-12 12:22:53 +0530

    Third review

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "0 to 30",
+    array: [0, 30],
+  },
+  {
+    _id: 1,
+    name: "31 to 60",
+    array: [31, 60],
+  },
+  {
+    _id: 2,
+    name: "61 to 90",
+    array: [61, 90],
+  },
+  {
+    _id: 3,
+    name: "91 to 120",
+    array: [91, 120],
+  },
+  {
+    _id: 4,
+    name: "121 to 150",
+    array: [121, 150],
+  },
+  {
+    _id: 4,
+    name: "151 or more",
+    array: [151, 9999],
+  },
+];
